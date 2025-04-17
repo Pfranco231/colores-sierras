704cafe5e2dbf6d391832b7c25b0ea9d857d59b9
Franco Papeschi <papeschifranco@gmail.com>
2025-04-17 09:21:58 -0300

    Pozos listo v1.8

--- json/pozos.js
+++ json/pozos.js
@@ -1,13 +1,12 @@
 const placespozos = [
     {
-        name: "PLAZA LA PAZ",
-        desp: "LA PLAZA SAN MARTIN DE LA PAZ ES UNA DE LAS MEJORES",
-        img_1: "ETC",
-        img_2: "etc",
-        img_3: "etc",
-        img_4: "etc",
-        img_5: "etc",
-        location: "etc",
+        id: 1,
+        name: "Dique La Viña",
+        desp: "Dique la viña : A tan sólo 25 km de Mina clavero se puede encontrar este maravilloso paredón de concreto entre un majestuoso cañadón natural. Es una obra de ingeniería moderna digna de apreciar y disfrutar y para quienes pueden mirar por sus 100mtr. sin sufrir vértigo se sugiere hacerlo con precaución. ",
+        img_1: "../imgs/turisticos/pozos/1/3murallon.jpg",
+        img_2: "../imgs/turisticos/pozos/1/murallon.jpg",
+        img_3: "../imgs/turisticos/pozos/1/1murallon.jpg",
+        location: "https://www.google.com/maps/search/?api=1&query=-31.871853,-65.035526",
     },
 ];
 
