0f3d89d6f57fbd9b89f98783fa7c66c434701b4c
Franco Papeschi <papeschifranco@gmail.com>
2025-04-17 08:57:59 -0300

    Rosas Listo V1.7

--- json/rosas.js
+++ json/rosas.js
@@ -1,13 +1,12 @@
 const placesrosas = [
     {
-        name: "PLAZA LA PAZ",
-        desp: "LA PLAZA SAN MARTIN DE LA PAZ ES UNA DE LAS MEJORES",
-        img_1: "ETC",
-        img_2: "etc",
-        img_3: "etc",
-        img_4: "etc",
-        img_5: "etc",
-        location: "etc",
+        id: 1,
+        name: "Plaza de Villa de las Rosas",
+        desp: "Todos los sábados desde las 11 y hasta las 16 hay fiesta; es de día y en la plaza. Se trata de La Feria de Productores y Artesanos de Villa de Las Rosas: unos 15 años atrás se instalaban los primeros tres o cuatro puestos; hoy es el paseo de compras más simpático, convocante y colorido del valle. Y no exageramos...",
+        img_1: "../imgs/turisticos/rosas/1/comidas-arabes.jpg",
+        img_2: "../imgs/turisticos/rosas/1/mas-comida.jpg",
+        img_3: "../imgs/turisticos/rosas/1/puestos.jpg",
+        location: "https://www.google.com/maps/search/?api=1&query=-31.947037,-65.052095",
     },
 ];
 
