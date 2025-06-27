c3b9978489db4ed81cc7dcc509209f35a2f76962
Franco Papeschi <papeschifranco@gmail.com>
2025-06-27 15:26:39 -0300

    TODOS LOS LUGARES YA DEFINIDOS

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ import placeslapaz from "../json/lapaz.js"
 import placesbrochero from "../json/brochero.js"
 import placesrabonas from "../json/lasrabonas.js"
 import placeslomabola from "../json/lomabola.js"
-import placesmerlo from "../json/merlo.js"
 import placesclavero from "../json/minaclavero.js"
 import placesnono from "../json/nono.js"
 import placespozos from "../json/pozos.js"
@@ -13,7 +12,6 @@ import placessjavier from "../json/sanjavier.js"
 let main = document.getElementById('main')
 let onplaces1 = document.getElementById('onplaces1')
 let onplaces2 = document.getElementById('onplaces2')
-let onplaces3 = document.getElementById('onplaces3')
 let onplaces4 = document.getElementById('onplaces4')
 let onplaces5 = document.getElementById('onplaces5')
 let onplaces6 = document.getElementById('onplaces6')
@@ -124,60 +122,6 @@ onplaces2.addEventListener("click",()=>{
 
     divContainer.append(...cards)
     main.appendChild(divContainer)
-    
-})
-
-onplaces3.addEventListener("click",()=>{
-    console.log("Ejecutando merlo")
-    main.innerHTML = " "
-    const divContainer = document.createElement('div')
-    divContainer.style.display = 'flex';
-    divContainer.style.marginTop = '20px';
-    divContainer.style.justifyContent = 'center';
-    divContainer.style.flexWrap = 'wrap';
-    divContainer.style.gap = '15px';
-
-
-
-    // Ejecucion de div y Cards
-    
-    const cards = placesmerlo.map(item => {
-        const divCard = document.createElement('div');
-        divCard.className = 'card';
-        divCard.style.height = '370px';
-        divCard.style.width = '18rem';
-        divCard.style.backgroundColor = '#FFFFFF';
-        divCard.style.borderRadius = '15px';
-        
-        const img = document.createElement('img');
-        img.setAttribute("loading", "lazy");
-        img.className = 'card-img-top';
-        img.borderRadius = '10px';
-        img.src = item.img_1;
-        divCard.appendChild(img);
-
-        const divContent = document.createElement('div')
-        divContent.className = 'card-body';
-        divCard.appendChild(divContent)
-
-        const cardTitle = document.createElement('h5')
-        cardTitle.className = 'card-title';
-        cardTitle.innerText = item.name;
-        divContent.appendChild(cardTitle)
-
-        const cardA = document.createElement('a')
-        cardA.classList = 'btn btn-primary';
-        cardA.style.border = 'none';
-        cardA.href = '#';
-        cardA.innerText = "Ver mas";
-        divContent.appendChild(cardA)
-        
-        return divCard;
-    })
-
-    divContainer.append(...cards)
-    main.appendChild(divContainer)
-    
     
 })
 
